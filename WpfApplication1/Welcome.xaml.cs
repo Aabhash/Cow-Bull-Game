a0be409a516cbfb6539c85cc7a98a01bce29fd64
Aabhash <ad_flint@hotmail.com>
2016-11-18 06:46:58 +0545

    Change MainWindow.xaml to Welcome.xaml

--- WpfApplication1/Welcome.xaml.cs
+++ WpfApplication1/Welcome.xaml.cs
@@ -17,9 +17,9 @@ namespace WpfApplication1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Welcome : Window
     {
-        public MainWindow()
+        public Welcome()
         {
             InitializeComponent();
            // PlaySound();            
